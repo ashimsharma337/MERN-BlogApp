a3ac810d15eb077d3fc913c7286c8cf3198a5c98
Ashim Sharma <ashimsharma337@gmail.com>
2022-05-18 11:44:39 -0400

    Settings component completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Topbar from "./components/topbar/Topbar";
+import Setting from "./components/pages/settings/Settings";
 
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
-      <Write/>
+      <Setting/>
     </div>
   );
 }
