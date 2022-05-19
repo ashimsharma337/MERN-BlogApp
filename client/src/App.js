a461292c272be6963ee202828c37b4d1f4da7678
Ashim Sharma <ashimsharma337@gmail.com>
2022-05-19 12:24:03 -0400

    login and register page completed

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Topbar from "./components/topbar/Topbar";
 import Setting from "./components/pages/settings/Settings";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
 
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
-      <Setting/>
+      <Register/>
     </div>
   );
 }
