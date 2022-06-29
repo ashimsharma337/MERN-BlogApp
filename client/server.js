afb9a24ef3bf9fcd17cf38869272564eba294e7d
Ashim Sharma <ashimsharma337@gmail.com>
2022-06-29 12:03:41 -0400

    server.js and readme updated

--- client/server.js
+++ client/server.js
@@ -4,15 +4,17 @@ const path = require("path");
 const app = express();
 
 
+app.use(express.static(path.join(process.cwd(), "build")));
 
 app.use("/", function(req, res, next){
     res.sendFile(path.join(process.cwd(), "build/index.html"));
 });
 
-app.use(express.static(path.join(process.cwd(), "build")));
+
 
 app.listen(process.env.PORT || 8080, function(err) {
     if(!err){
         console.log("Server is listening...");
     }
 })
+
